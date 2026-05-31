48a9385e86deb3f69cc8ca8d0dfff449c98b9c7e
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-31 08:17:58 +0100

    fix: pin atom source to commit hash, regenerate docs
    - Pin distribution module source to ref=a746ce6 (fixes terraform_module_pinned_source lint)
    - Regenerate terraform-docs to include custom_error_responses variable

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "cache_policy" {
 }
 
 module "distribution" {
-  source = "git::https://github.com/PlatformStackPulse/tf-atom-cloudfront-distribution-aws.git?ref=main"
+  source = "git::https://github.com/PlatformStackPulse/tf-atom-cloudfront-distribution-aws.git?ref=a746ce6"
 
   context              = module.this.context
   distribution_enabled = true
